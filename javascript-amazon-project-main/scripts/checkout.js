807bf37afe0f2fbad0390b8db5840530e53424cd
Anghelusjr <anghelusjr09.ab@gmail.com>
2025-06-06 15:43:10 +0800

    fix price formating and radio button

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let matchingItem;
 
@@ -26,7 +27,7 @@ cart.forEach((cartItem) =>{
                   ${product.name}
                 </div>
                 <div class="product-price">
-                  $${product.priceCents / 100}
+                  $${formatCurrency(product.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -46,9 +47,9 @@ cart.forEach((cartItem) =>{
                   Choose a delivery option:
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" checked
+                  <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -61,7 +62,7 @@ cart.forEach((cartItem) =>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -74,7 +75,7 @@ cart.forEach((cartItem) =>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
